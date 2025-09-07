043cdf4a9d5f5a2c16aeee0e7265a9a6a88f12a5
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-09-07 18:45:20 +0100

    added search fields

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,4 +5,5 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year",)
     list_filter = ("title", "author", "publication_year",)
+    search_fields = ("title", "author", "publication_year",)
 admin.site.register(Book, BookAdmin)
--- Introduction_to_Django/LibraryProject/bookshelf/create.md
+++ Introduction_to_Django/LibraryProject/bookshelf/create.md
@@ -1,5 +1,7 @@
 from bookshelf.models import Book
 
+Book.objects.create
+
 bookshelf = Book(title='1984',author='George Orwell', publication_year=1949)
 bookshelf.save()
 
