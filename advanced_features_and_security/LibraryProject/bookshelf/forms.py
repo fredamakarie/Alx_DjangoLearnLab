4354a6597d1cb9aa6a31b8b839650d0e53b561ab
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-09-18 07:34:38 +0100

    used exampleform in views

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,7 +2,8 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title", "author"]   # include only the fields you want users to edit
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
+from .forms import ExampleForm
 
+# List all books
 def book_list(request):
-      """Retrieves all books and renders a template displaying the list."""
-      books = Book.objects.all()  # Fetch all book instances from the database
-      context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'bookshelf/book_list.html', context)
+    books = Book.objects.all()
+    return render(request, "books/book_list.html", {"books": books})
+
+# Create new book
+def book_create(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm()
+    return render(request, "books/book_form.html", {"form": form})
+
+# Edit book
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = ExampleForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm(instance=book)
+    return render(request, "books/book_form.html", {"form": form})
+
+# Delete book
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "books/book_confirm_delete.html", {"book": book})
+
 
 
 # View books
