32a038c798968922f25871f11a91d217bb322e76
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-09-18 03:46:43 +0100

    api setup, urls and views for rest framework

---
+++ api_project/api_project/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Book
+from rest_framework import generics,viewsets
+from .serializers import BookSerializer
+
+# Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
