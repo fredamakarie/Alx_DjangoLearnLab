6833b4068e8edab01b684b90ea046e3964c060c5
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-10-02 17:21:14 +0100

    fixed files to be ignored

--- django_blog/django_blog/blog/models.py
+++ django_blog/django_blog/blog/models.py
@@ -20,8 +20,8 @@ class Comment():
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author= models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(autp_now=True)
 
     def __str__(self):
         return self.content
